d59ab357d2f53b165d015628f8a3d4d6e3f2c013
tatsu1109 <ts11252tt1109@gmail.com>
2021-01-11 00:24:54 +0900

    共有していない関数をインラインに移動

--- src/views/pages/App.js
+++ src/views/pages/App.js
@@ -8,8 +8,6 @@ import AudioPlayer from '../components/AudioPlayer'
 import IconButton from '../components/IconButton'
 
 const App = () => {
-  const [audioList, setAudioList] = useState([])
-  const [selectedIndex, setSelectedIndex] = useState(0)
   const [recording, setRecording] = useState('inactive')
   const [fileName, setFileName] = useState('sample')
   const [audio, setAudio] = useState(null)
@@ -17,19 +15,12 @@ const App = () => {
   // https://ja.reactjs.org/docs/hooks-reference.html#useref
   const recordRef = useRef(null)
 
+  const [audioList, setAudioList] = useState([])
+  const [selectedIndex, setSelectedIndex] = useState(0)
   const handleAudioListItemClick = ({id, audio}) => {
     setSelectedIndex(id)
     setAudio(audio)
   };
-
-  const handleAudioListDeleteClick = (list, { id }) => {
-    console.log(id)
-    setAudioList(list.filter(item => item.id !== id))
-  };
-
-  const handleRecordingButtonOnClick = () => {
-    recording === 'inactive' ? startRecording() : recordRef.current.stop()
-  }
 
   const startRecording = () => {
     // メディア入力デバイスへのアクセスを起動する、Promiseを作成
@@ -53,16 +44,13 @@ const App = () => {
       }
     })
 
-    recordRef.current.addEventListener('start', () => {
-      setRecording(recordRef.current.state)
-    })    
+    recordRef.current.addEventListener('start', () => setRecording(recordRef.current.state))    
 
     recordRef.current.addEventListener('stop', () => {
-      const audio = URL.createObjectURL(new Blob(tempChunks))
       const audioListItem = {
         id: audioList.length === 0 ? 1 : audioList.flatMap(item => item.id).reduce((a, b) => Math.max(a, b)) + 1,
         name: fileName,
-        audio: audio
+        audio: URL.createObjectURL(new Blob(tempChunks))
       }
       handleAudioListItemClick(audioListItem)
       setAudioList([...audioList, audioListItem])
@@ -89,7 +77,7 @@ const App = () => {
           onChange={event => setFileName(event.target.value)} />
         <IconButton
           color='secondary'
-          handleClick={handleRecordingButtonOnClick}
+          handleClick={recording === 'inactive' ? startRecording() : recordRef.current.stop()}
           icon={recording === 'inactive' ? <KeyboardVoiceIcon /> : <StopIcon />} 
         />
       </div>
@@ -97,7 +85,7 @@ const App = () => {
         <SelectedList
           list={audioList}
           handleItemClick={handleAudioListItemClick}
-          handleDeleteClick={handleAudioListDeleteClick}
+          handleDeleteClick={(list, { id }) => setAudioList(list.filter(item => item.id !== id))}
           index={selectedIndex} />
       </div>
     </div>
